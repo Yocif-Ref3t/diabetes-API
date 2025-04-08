1b1a88a3ee2083addaa8843b0432c0bdc02e6378
Youssef Refaat <dev.yossifrefaat@gmail.com>
2025-04-08 19:13:14 +0200

    change port

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import pickle
 import numpy as np
+import os
 from flask import Flask, request, jsonify
 
 with open("diabetes-model.pkl", "rb") as file:
@@ -20,4 +21,5 @@ def predict():
         return jsonify({"error": str(e)}), 400  
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
